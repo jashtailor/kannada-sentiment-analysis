36a6066168cd36785e66f19a19600f09953adb07
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-04-10 21:43:17 +0530

    Update nlp_mini_project.py

--- nlp_mini_project.py
+++ nlp_mini_project.py
@@ -27,42 +27,34 @@ with open('dictionary.json', 'r') as fp:
 clf = pickle.load(open('model.pkl', 'rb'))
 
 def prediction(input):
-  longest = 2875
-  lst5 = input.split(' ')
-  lst6 = []
-  for each in lst5:
-    lst6.append(data[each])
-  if len(lst6)<longest:
-    lst8 = lst6 + ([0]*(longest-len(lst6)))
-  lst9 = np.asarray(lst8, dtype=np.float64)
-  lst7 = lst9.reshape(1, -1)
-  a = clf.predict(lst7)
-  # print(a)
-  st.write(a)
-  '''
-  if a==1:
-    st.write('Positive Sentiment')
-  elif a==0:
-    st.write('Negative Sentiment')
-  '''
-  return (a)
-'''     
+    longest = 2875
+    lst5 = input.split(' ')
+    lst6 = []
+    for each in lst5:
+        lst6.append(data[each])
+        # print(data[each])
+    # print(lst6)
+    if len(lst6)<longest:
+        lst8 = lst6 + ([0]*(longest-len(lst6)))
+    # print(lst8)
+    lst9 = np.asarray(lst8, dtype=np.float64)
+    lst7 = lst9.reshape(1, -1)
+    a = clf.predict(lst7)
+    # print(type(a))
+    if a==1:
+        print('hi')
+    # print(a)
+    # print(type(a))
+    # print(a[0])
+    if a[0]==1:
+        print('positive')
+    else:
+        print('negative')
+    
 if st.button(label="Submit"):
   try:
-    b = prediction(text)
-    if b==1:
-      st.write('Positive Sentiment')
-    elif b==0:
-      st.write('Negative Sentiment')
+    prediction(text)
   except:
     pass
 else:
   pass
-'''
-
-if st.button(label='Submit'):
-  b = prediction(text)
-  if b==1:
-    st.write('Positive Sentiment')
-  elif b==0:
-    st.write('Negative Sentiment')
